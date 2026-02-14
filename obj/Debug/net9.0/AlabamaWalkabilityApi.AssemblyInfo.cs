cc87eaddf0f68812f32ee5ac71eb4a7cc5c13ead
RileyDavisGilbreath <rdgilbreath@crimson.ua.edu>
2026-02-14 14:33:55 -0600

    fix push

--- obj/Debug/net9.0/AlabamaWalkabilityApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/AlabamaWalkabilityApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlabamaWalkabilityApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4936e4930bf00086891fed7803f2ab0abaa6ad4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4351eadf7809818c501aebfa5fae14c408e2ee8")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlabamaWalkabilityApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlabamaWalkabilityApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
